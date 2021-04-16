b33ca1efb701cfa302d45692108fb32cbc2665e8
Sherpad Ndabambi <sgndabambi@gmail.com>
2021-04-16 20:45:26 +0200

    Changed shading

--- files/js/display.js
+++ files/js/display.js
@@ -2,46 +2,50 @@ function shadeText() {
 	let changedTextArray = changedText.innerHTML.split("<br>"), originalTextArray = originalText.innerHTML.split("<br>");
 	let shortestText = changedTextArray.length <= originalTextArray.length ? changedTextArray : originalTextArray;
 	let longestText = changedTextArray.length > originalTextArray.length ? changedTextArray : originalTextArray;
+	let changedTextString  = '', originalTextString = '';
 
-	changedText.innerHTML = '';
-	originalText.innerHTML = '';
 	for(let i = 0; i < shortestText.length; i++){
 		if(shortestText[i] == longestText[i]){
-			changedText.innerHTML += changedTextArray[i] + "<br>";
-			originalText.innerHTML +=  originalTextArray[i] + "<br>";
+			changedTextString += changedTextArray[i] + "<br>";
+			originalTextString +=  originalTextArray[i] + "<br>";
 		}
 		else{
 			changedTextArray[i] = changedTextArray[i].split(' ');
 			originalTextArray[i] = originalTextArray[i].split(' ');
 			let shortestSentence = changedTextArray[i].length <= originalTextArray[i].length ? changedTextArray[i] : originalTextArray[i];
 			let longestSentence = changedTextArray[i].length > originalTextArray[i].length ? changedTextArray[i] : originalTextArray[i];
-			//changedText.innerHTML += "<div style='background-color:pink'>";
-			//originalText.innerHTML += "<div style='background-color:powderblue'>";
+			changedTextString += "<div style='background-color:pink'>";
+			originalTextString += "<div style='background-color:powderblue'>";
 			for(let j = 0; j < shortestSentence.length; j++){
 				if(shortestSentence[j] == longestSentence[j]){
-					changedText.innerHTML += "<span style='background-color:pink'>" + changedTextArray[i][j] + " </span>";
-					originalText.innerHTML += "<span style='background-color:powderblue'>" + originalTextArray[i][j] + " </span>";
+					changedTextString += changedTextArray[i][j] + " ";
+					originalTextString += originalTextArray[i][j] + " ";
 				}
 				else{
-					changedText.innerHTML += "<span style='background-color:lightcoral'>" + changedTextArray[i][j] + " </span>";
-					originalText.innerHTML += "<span style='background-color:mediumaquamarine'>" + originalTextArray[i][j] + " </span>";
+					changedTextString += "<span style='background-color:lightcoral'>" + changedTextArray[i][j] + " </span>";
+					originalTextString += "<span style='background-color:mediumaquamarine'>" + originalTextArray[i][j] + " </span>";
 				}
 			}
-			let long = changedTextArray[i].length == longestSentence.length ? changedText : originalText;
-			//long.innerHTML += "<span style='background-color:pink'>";
-			for(let j = shortestSentence.length; j < longestSentence.length; j++){
-				if(long == changedText) long.innerHTML += "<span style='background-color:pink'>" + longestSentence[j] + " </span>";
-				else long.innerHTML += "<span style='background-color:powderblue'>" + longestSentence[j] + " </span>";
+			if(changedTextArray[i].length == longestSentence.length){
+				changedTextString += "<span style='background-color:lightcoral'>";
+				for(let j = shortestSentence.length; j < longestSentence.length; j++) changedTextString += longestSentence[j] + " ";
+				changedTextString += "</span>";
 			}
-			//long.innerHTML += "</span>";
-			//changedText.innerHTML += "</div><br>";
-			//originalText.innerHTML += "</div><br>";
-			changedText.innerHTML += "<br>";
-			originalText.innerHTML += "<br>";
+			else{
+				originalTextString += "<span style='background-color:mediumaquamarine'>";
+				for(let j = shortestSentence.length; j < longestSentence.length; j++) originalTextString += longestSentence[j] + " ";
+				originalTextString += "</span>";
+			}
+			changedTextString += "</div><br>";
+			originalTextString += "</div><br>";
 		}
 	}
-	let long = changedTextArray.length == longestText.length ? changedText : originalText;
-	for(let i = shortestText.length; i < longestText.length; i++) long.innerHTML += longestText[i] + "<br>";
+
+	if(changedTextArray.length == longestText.length) for(let i = shortestText.length; i < longestText.length; i++) changedTextString += "<div style='background-color:pink'><span style='background-color:lightcoral'>" + longestText[i] + "</span></div><br>";
+	else for(let i = shortestText.length; i < longestText.length; i++) originalTextString += "<div style='background-color:powderblue'><span style='background-color:mediumaquamarine'>" + longestText[i] + "</span></div><br>";
+
+	changedText.innerHTML = changedTextString;
+	originalText.innerHTML = originalTextString;
 }
 
 function onPageLoad() {
